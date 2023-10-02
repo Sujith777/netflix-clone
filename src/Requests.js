bd2af9c05789e827d6ce045699592d911e4088d5
bspraveen <billasujith77@gmail.com>
2023-10-02 12:37:54 +0530

    Adding environment variables and changing some api endpoints

--- src/Requests.js
+++ src/Requests.js
@@ -1,11 +1,11 @@
 const key = process.env.REACT_APP_TMDB_API_KEY;
-export const token =
-  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMjJiMzZmOTUwNDU2MTc1ZjllMTNhNzZiYjJiOWY0MiIsInN1YiI6IjY1MTgwZDViYTE5OWE2MDEzOGI4YThmYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9_OVR568RvlvbYQ7EW4lM8qscH1pU9xvYnnm3ofBzbw";
+export const token = process.env.REACT_APP_TMDB_ACCESS_TOKEN;
 
 const requests = {
   requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
   requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
-  requestTrending: `https://api.themoviedb.org/3/movie/now_playing/?api_key=${key}&language=en-US&page=1`,
+  requestTrending: `
+  https://api.themoviedb.org/3/trending/movie/week?api_key=${key}&language=en-US&page=1`,
   requestUpComing: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
       <Main />
       <Row rowID="1" title="Upcoming" fetchURL={requests.requestUpComing} />
       <Row rowID="2" title="Popular" fetchURL={requests.requestPopular} />
-      <Row rowID="3" title="Now Playing" fetchURL={requests.requestTrending} />
+      <Row rowID="3" title="Trending" fetchURL={requests.requestTrending} />
       <Row rowID="4" title="Top Rated" fetchURL={requests.requestTopRated} />
     </>
   );
